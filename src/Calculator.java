41b0d55131ca05a10d27eefc172fdae7f98d487e
Innominate-Dev <InnominateDev@gmail.com>
2024-09-08 00:44:43 +0100

    Added the calculator functions

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,17 @@
+import java.util.Scanner; // import the scanner class
+
 public class Calculator {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+
+        int num1 = userInput.nextInt();
+
+        System.out.println("Enter your second number: ");
+
+        int num2 = userInput.nextInt();
+
+        System.out.println("Your two numbers add up to " + (num1 + num2)); // adding numbers
+        userInput.close();
     }
 }
