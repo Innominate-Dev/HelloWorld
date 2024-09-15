519246b44711e0fd9c0a5d6e01a598d11a6b9ac5
Innominate-Dev <InnominateDev@gmail.com>
2024-09-15 02:43:49 +0100

    Added on and off system for the calculator and user can input the type of operator they'd like to use.

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,16 +2,57 @@ import java.util.Scanner; // import the scanner class
 
 public class Calculator {
     public static void main(String[] args) throws Exception {
+        boolean turnOffCal = false;
+
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        
+        while (turnOffCal == false) {
+            System.out.println("Enter a number: ");
 
-        int num1 = userInput.nextInt();
+            int num1 = userInput.nextInt();
 
-        System.out.println("Enter your second number: ");
+            System.out.println("what operator would you like to use? (* , / , +, - )");
 
-        int num2 = userInput.nextInt();
+            char operator = userInput.next().charAt(0);
 
-        System.out.println("Your two numbers add up to " + (num1 + num2)); // adding numbers
-        userInput.close();
+            System.out.println("Enter your second number: ");
+
+            int num2 = userInput.nextInt();
+
+            if(operator == '/')
+            {
+                System.out.println("Your two numbers divide up to " + (num1 / num2)); // dividing numbers
+            }
+            else if(operator == '*')
+            {
+                System.out.println("Your two numbers multiply to " + (num1 * num2)); // multiply numbers
+            }
+            else if(operator == '+')
+            {
+                System.out.println("Your two numbers add up to " + (num1 + num2)); // adding numbers
+            }
+            else if(operator == '-')
+            {
+                System.out.println("Your two numbers take away to " + (num1 - num2)); // take away numbers
+            }
+            else{
+                System.out.println("Fatal Error calculation could not be made.");
+            }
+
+            System.out.println("Would you like to continue using THE calculator (y/n)");
+            char continueUsingCal = userInput.next().charAt(0);
+
+            if(continueUsingCal == 'y')
+            {
+                continue;
+            }
+            else if(continueUsingCal == 'n')
+            {
+                System.out.println("Thank you for using THE calculator :]");
+                turnOffCal = true;
+                userInput.close();
+                break;
+            }
+        }
     }
 }
